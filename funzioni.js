deafc7a5285e0b662bf642962c3c58d0da180fc8
Lisuccia00 <110474046+Lisuccia00@users.noreply.github.com>
2023-03-21 21:32:57 +0100

    primo
    first

---
+++ funzioni.js
@@ -0,0 +1,7 @@
+function accetta(){
+    document.getElementById("risposta").innerHTML = "GRAZIE MILLE TI AMO <3"
+}
+
+function rifiuta(){
+    document.getElementById("risposta").innerHTML = "AH OK VA BENE LO STESSO :("
+}
